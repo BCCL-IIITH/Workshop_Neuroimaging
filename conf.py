62c3f7c821af83e122f2146dd2a21e8132e1f825
KamalakerDadi <dkamalakarreddy@gmail.com>
2024-07-10 12:39:40 +0200

    workshop logo

--- conf.py
+++ conf.py
@@ -52,7 +52,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "**.ipynb_checkpoints"]
 #
 html_title = ""
 html_theme = "sphinx_book_theme"
-html_logo = "_static/nipy_logo.png"
+html_logo = "_static/logo.png"
 html_theme_options = {
     "github_url": "https://github.com/BCCL-IIITH/Workshop_Neuroimaging",
     "repository_url": "https://github.com/BCCL-IIITH/Workshop_Neuroimaging",
